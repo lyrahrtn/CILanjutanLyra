edfdd4ab1a6aad62b5aaaf117652cd8cf712530f
Lyra Hertin <Lyra Hertin>
2019-01-22 14:14:31 +0700

    selesai

---
+++ application/views/event.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>Register</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?= base_url()?>assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="<?= base_url()?>assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/js/bootstrap-datepicker/css/datepicker.css" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/js/bootstrap-daterangepicker/daterangepicker.css" />
+        
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="<?= base_url()?>assets/css/events.css">
+    <link href="<?= base_url()?>assets/css/style.css" rel="stylesheet">
+    <link href="<?= base_url()?>assets/css/style-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+      <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">
+          <body class="pop">
+        <img style="width: 400px; margin-top: 50px; margin-right: auto; margin-left: auto; display: block;" src="<?=base_url()?>assets/img/song.jpg" alt="">    
+         <h2>Designing a house with Song Couple</h2> 
+         <p id="event">
+             Watch it on channel KBS on Saturday, January 26 2019 at 7.00 p.m. Have fun to decorate the home of this universal couple. Stay tune!
+        </p>
+          	
+		</section><! --/wrapper -->
+      </section><!-- /MAIN CONTENT -->
+
+      <!--main content end-->
+  </body>
+</html>
